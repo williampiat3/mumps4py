ead1ed500cf7a407ee7d91b20ab280f9d1546125
William PIAT <william.piat@safrangroup.com>
2025-08-12 18:40:26 +0200

    try to adapt for win

--- tests/test_dmumps.py
+++ tests/test_dmumps.py
@@ -3,15 +3,17 @@ import pytest
 import os
 from mpi4py import MPI
 from mumps4py.mumps_solver import MumpsSolver
+import platform
 
 # @pytest.mark.skipif("dmumps" not in os.getenv("MUMPS_SOLVERS", "").split(","), reason="dmumps not selected")
 def test_solve_double():
     solver = MumpsSolver(verbose=False, system="double")
-
+    SYSTEM = platform.system().lower()
+    inttype = np.int32 if SYSTEM != "windows" else np.int64
     dtype = np.float64
     n = 5
-    irn = np.array([0,1,3,4,1,0,4,2,1,2,0,2], dtype=np.int32)
-    jcn = np.array([1,2,2,4,0,0,1,3,4,1,2,2], dtype=np.int32)
+    irn = np.array([0,1,3,4,1,0,4,2,1,2,0,2], dtype=inttype)
+    jcn = np.array([1,2,2,4,0,0,1,3,4,1,2,2], dtype=inttype)
     a = np.array([3.0,-3.0,2.0,1.0,3.0,2.0,4.0,2.0,6.0,-1.0,4.0,1.0], dtype=dtype)
     b = np.array([20.0,24.0,9.0,6.0,13.0], dtype=dtype)
 
