00e1cb6775af69ac12f69858d5f100a5b15a783f
William PIAT <william.piat@safrangroup.com>
2025-08-13 10:02:52 +0200

    revert tests

--- tests/test_zmumps.py
+++ tests/test_zmumps.py
@@ -3,18 +3,16 @@ import pytest
 import os
 from mpi4py import MPI
 from mumps4py.mumps_solver import MumpsSolver
-import platform
 
 # @pytest.mark.skipif("zmumps" not in os.getenv("MUMPS_SOLVERS", "").split(","), reason="zmumps not selected")
 def test_solve_single():
     solver = MumpsSolver(verbose=False, system="complex128")
-    SYSTEM = platform.system().lower()
-    inttype = np.int32 if SYSTEM != "windows" else np.int64
+
     dtype = np.complex128
 
     n = 5
-    irn = np.array([0,1,3,4,1,0,4,2,1,2,0,2], dtype=inttype)
-    jcn = np.array([1,2,2,4,0,0,1,3,4,1,2,2], dtype=inttype)
+    irn = np.array([0,1,3,4,1,0,4,2,1,2,0,2], dtype=np.int32)
+    jcn = np.array([1,2,2,4,0,0,1,3,4,1,2,2], dtype=np.int32)
     a = np.array([3.0,-3.0,2.0,1.0,3.0,2.0,4.0,2.0,6.0,-1.0,4.0,1.0], dtype=dtype)
     b = np.array([20.0,24.0,9.0,6.0,13.0], dtype=dtype)
 
